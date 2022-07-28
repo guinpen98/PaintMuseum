7673cacd351d7dd31f9b40dea80c465b5630a79b
guinpen98 <yuri.pocha.0908@gmail.com>
2022-07-29 00:26:20 +0900

    implement UnityChan animation

--- Assets/Project/Program/Yu-ri/Scripts/PlayerMove.cs
+++ Assets/Project/Program/Yu-ri/Scripts/PlayerMove.cs
@@ -9,29 +9,41 @@ public class PlayerMove : MonoBehaviour
     [Header("移動の速さ"), SerializeField]
     private float _speed = 3;
 
-    private Transform _transform;
     private CharacterController _characterController;
 
     private Vector2 _inputMove;
     private float _verticalVelocity;
-    private float _turnVelocity;
+
+    Animator anima;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         // 入力値を保持しておく
         _inputMove = context.ReadValue<Vector2>();
+        setAnima();
+    }
+    void setAnima()
+    {
+        if (_inputMove.x == 0 && _inputMove.y == 0) anima.SetFloat("Speed", 0f);
+        else anima.SetFloat("Speed", 1f);
+        if (_inputMove.x != 1.0f && _inputMove.x != -1.0f) return;
+        Vector3 scale = transform.localScale;
+        scale.x = _inputMove.x;
+        transform.localScale = scale;
     }
 
-
-    private void Awake()
+    private void Start()
     {
-        _transform = transform;
         _characterController = GetComponent<CharacterController>();
+        anima = GetComponent<Animator>();
     }
 
     private void Update()
     {
-
+        Move();
+    }
+    void Move()
+    {
         // 操作入力と鉛直方向速度から、現在速度を計算
         var moveVelocity = new Vector3(
             _inputMove.x * _speed,
@@ -43,25 +55,5 @@ public class PlayerMove : MonoBehaviour
 
         // CharacterControllerに移動量を指定し、オブジェクトを動かす
         _characterController.Move(moveDelta);
-
-        if (_inputMove != Vector2.zero)
-        {
-            // 移動入力がある場合は、振り向き動作も行う
-
-            // 操作入力からy軸周りの目標角度[deg]を計算
-            var targetAngleY = -Mathf.Atan2(_inputMove.y, _inputMove.x)
-                * Mathf.Rad2Deg + 90;
-
-            // イージングしながら次の回転角度[deg]を計算
-            var angleY = Mathf.SmoothDampAngle(
-                _transform.eulerAngles.y,
-                targetAngleY,
-                ref _turnVelocity,
-                0.1f
-            );
-
-            // オブジェクトの回転を更新
-            _transform.rotation = Quaternion.Euler(0, angleY, 0);
-        }
     }
 }
