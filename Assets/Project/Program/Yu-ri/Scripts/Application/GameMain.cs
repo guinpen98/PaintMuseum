bc24b715167dd77c7cfc0ec6fffe88576e53475b
guinpen98 <yuri.pocha.0908@gmail.com>
2022-10-12 01:47:37 +0900

    Implement a door that opens when the player approaches the door and presses the E key, and closes when the player moves away from the door

--- Assets/Project/Program/Yu-ri/Scripts/Application/GameMain.cs
+++ Assets/Project/Program/Yu-ri/Scripts/Application/GameMain.cs
@@ -26,6 +26,7 @@ public class GameMain : MonoBehaviour
             new AnimationSystem(),
             new WarpSystem(),
             new FadeSystem(),
+            new InputSystem(),
         };
 
         foreach (BaseSystem system in _systemList)
