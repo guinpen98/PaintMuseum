59be97cb26bdb79c76f0782b6e2bc0716825f156
guinpen98 <yuri.pocha.0908@gmail.com>
2022-10-15 23:42:27 +0900

    Implement key system

---
+++ Assets/Project/Program/Yu-ri/Scripts/Entity/KeyEntity.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class KeyEntity : MonoBehaviour
+{
+    public Action getKey;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")
+        {
+            getKey.Invoke();
+        }
+    }
+}
